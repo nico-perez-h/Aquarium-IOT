ed738278b7b4308eb2bff8decceb0f530e09f5a4
Nico Perez <nicoherrera481@gmail.com>
2024-10-14 11:15:35 -0400

    Primer commit: inicialización del proyecto

---
+++ app/home.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { useRouter } from 'expo-router'
+
+const Home = () => {
+    const router = useRouter();
+    return (
+        <View>
+            <Text>Ventana home</Text>
+            <TouchableOpacity onPress={() => router.push("/")}><Text>Index</Text></TouchableOpacity>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
