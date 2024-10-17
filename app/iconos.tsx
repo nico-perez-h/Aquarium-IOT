832ef8a163d9a021b81b07c1d4cf048edeafca11
Nico Perez <nicoperez.dev@gmail.com>
2024-10-17 00:50:37 -0400

    Agregar e instalar librerias

---
+++ app/iconos.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { router } from 'expo-router'
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
+const Iconos = () => {
+  return (
+    <View>
+      <Text>Iconos</Text>
+      <TouchableOpacity onPress={() => router.push("/")}>
+        <Text>Index</Text>        
+      </TouchableOpacity>
+      <Icon name="arrow-left" size={30} color="black" />
+      <Icon name="facebook" size={30} color="black" />
+      <Icon name="google" size={30} color="#DB4437" />
+      <Icon name="apple" size={30} color="black" />
+      <Icon name="lemon" size={30} color="black" />
+      <Icon name="lemon" size={30} color="black"  solid/>
+      <Icon name="wifi" size={30} color="black" />
+      <Icon name="sun" size={30} color="#FFD700" solid />
+      <Icon name="react" size={30} color="black"/>  
+      <Icon name="6" size={30} color="blue" />
+    </View>
+  )
+}
+
+export default Iconos
+
+const styles = StyleSheet.create({})
