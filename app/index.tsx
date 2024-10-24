a6ad53de76e8b5e846216bb5f98c458e7865a7eb
Nico Perez <nicoherrera481@gmail.com>
2024-10-24 17:06:21 -0400

    Pruebas de ventanas

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,14 @@
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityComponent, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useNavigation, useRouter } from 'expo-router'
-import Nav from '../components/nav'
-const Index = () => {
-  const router = useRouter();
+
+const index = () => {
   return (
-    <View style={styles.contMain}>
-      <Text >Acuario IOT</Text>
-      <TouchableOpacity onPress={() => router.push("/home")}>
-        <Text style={styles.contButtonText}>Ir a home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/loginTab")}>
-        <Text style={styles.contButtonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/firstPage")}>
-        <Text style={styles.contButtonText}>Pagina de Incio</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/iconos")}>
-        <Text style={styles.contButtonText}>Pagina de Iconos</Text>
-      </TouchableOpacity>
-      <Nav/>
+    <View>
+      <Text>index</Text>
     </View>
   )
 }
 
-export default Index
+export default index
 
-const styles = StyleSheet.create({
-  contMain: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 375,
-    height: 710,
-  },
-  contButtonText: {
-    fontSize:20
-  }
-})
+const styles = StyleSheet.create({})
