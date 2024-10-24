81101dae128d7b56427f4471e38ca0bf2ef908f0
Nico Perez <nicoperez.dev@gmail.com>
2024-10-24 18:15:09 -0400

    Navbar con Tabs

--- app/screens/index..tsx
+++
@@ -1,4 +0,0 @@
-export {default as Account} from './Account'
-export {default as Dashboard} from './Dashboard'
-export {default as Notifications} from './Notifications'
-export {default as Inicio } from './Inicio'
