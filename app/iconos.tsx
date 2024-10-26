dbbd7ee55f5857bc45e318155a88a565c5c81769
Nico Perez <nicoherrera481@gmail.com>
2024-10-25 22:56:31 -0400

    Cambio nombre de la importancion de iconos

--- app/iconos.tsx
+++ app/iconos.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { router } from 'expo-router'
-import Icon from "react-native-vector-icons/FontAwesome6";
+import Material from "react-native-vector-icons/FontAwesome6";
 import Nav from '../components/nav';
 
 const Iconos = () => {
@@ -9,18 +9,18 @@ const Iconos = () => {
     <View>
       <Text>Iconos</Text>
       <TouchableOpacity onPress={() => router.push("/")}>
-        <Icon name="facebook" size={30} color="black" />
+        <Material name="facebook" size={30} color="black" />
       </TouchableOpacity>
-      <Icon name="arrow-left" size={30} color="black" />
+      <Material name="arrow-left" size={30} color="black" />
 
-      <Icon name="google" size={30} color="#DB4437" />
-      <Icon name="apple" size={30} color="black" />
-      <Icon name="lemon" size={30} color="black" />
-      <Icon name="lemon" size={30} color="black" solid />
-      <Icon name="wifi" size={30} color="black" />
-      <Icon name="sun" size={30} color="#FFD700" solid />
-      <Icon name="react" size={30} color="black" />
-      <Icon name="6" size={30} color="blue" />
+      <Material name="google" size={30} color="#DB4437" />
+      <Material name="apple" size={30} color="black" />
+      <Material name="lemon" size={30} color="black" />
+      <Material name="lemon" size={30} color="black" solid />
+      <Material name="wifi" size={30} color="black" />
+      <Material name="sun" size={30} color="#FFD700" solid />
+      <Material name="react" size={30} color="black" />
+      <Material name="6" size={30} color="blue" />
       <Nav/>
     </View>
   )
