1ae591b728af1106a622d56de1e5f42b27d201da
Nico Perez <nicoperez.dev@gmail.com>
2025-03-31 16:55:17 -0400

    Actualizacion de archivos

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,4 +1,4 @@
-import { Colors } from "@/constants/Colors";
+import { Colors } from "@/src/constants/Colors";
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
